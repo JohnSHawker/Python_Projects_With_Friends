2feeed4805b4808139f02eb6330afc470fef2039
John S. Hawker <JohnS.Hawker@gmail.com>
2021-03-10 23:39:41 -0700

    TEST 42

--- Test.py
+++ Test.py
@@ -1,3 +1,1 @@
-# Testing if this works to push and pull from my Windows Laptop VSCode to GitHub
-print("Test to add file from VSCode to GitHub")
-# something
+# Testing if this works to push and pull from my Windows Laptop VSCode to Git
