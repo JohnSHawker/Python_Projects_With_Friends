22f8e037fca0c921d6ae66925d72045a9b0920b4
Kameron Squire <itkametopaz@gmail.com>
2021-03-12 21:09:53 +0900

    made comment

--- Test.py
+++ Test.py
@@ -1,2 +1,4 @@
 # Testing if this works to push and pull from my Windows Laptop VSCode to Git
 # Hi Kameron Can you See this on Git Hub?
+
+#Hi Sol what is a project idea that you want to work on together?
