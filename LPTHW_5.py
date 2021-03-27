4ba9c8ecc87ede85193a279d016b4246232e5b05
John S. Hawker <JohnS.Hawker@gmail.com>
2021-03-26 19:08:04 -0600

    Sols TESTs

---
+++ LPTHW_5.py
@@ -0,0 +1,16 @@
+my_name = 'John Hawker'
+my_age = 31
+my_height = 181 #cm
+my_weight = 140 #kg
+my_eyes = 'Hazel'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} centimeters tall.")
+print(f"He's {my_weight} kilograms heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
