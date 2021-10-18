32fb87a43143261218d43371226ee2c6b65469b1
John S. Hawker <JohnS.Hawker@gmail.com>
2021-10-17 23:58:38 -0600

    Added a new file for helpful info

---
+++ visual_studio_code_shortcuts.py
@@ -0,0 +1,11 @@
+""" This page will be used to add a list of any VSCode shortcuts we might find useful or any helpful python information specifically
+best practices for python programming
+"""
+# when you want to comment out a bunch of code quickly first highlight the code then press "ctrl" + "/"
+# if you want to make a block comment in python use tripple double quotation marks
+"""Here is an example of block comments
+You will not be limited to only one line of text for your comment when using block comments.
+If you only need one line of comment use the octothorpe or hashtag (#).
+Best practice for a block comment is to have the trailing tripple double quotation marks on their own line once you have finished
+the comment block.
+"""
